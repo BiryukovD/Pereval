97c7614762c64bf5aeebd94624267f8246abe84a
root <root@qcujdakvat.local>
2024-02-06 16:24:03 +0000

    added cors

--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
 from fastapi import FastAPI
 from operations.router import router as router_operations
-
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from config import REDIS_HOST, REDIS_PORT
 
 app = FastAPI(title='App')
+
+origins = [
+        "http://localhost:3000",
+    "https://81.9.127.193"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
+    allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
+                   "Authorization"],
+)
 
 
 
